f0e171bbfa312141833954ce2d8b05aa0d74278a
jeff <jeff.min@healingpaper.com>
2022-03-25 17:15:18 +0900

    Ci Test 01

--- app/src/main/java/com/example/citesting/MainActivity.kt
+++ app/src/main/java/com/example/citesting/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 아무거나 수정
+        println("Hello")
     }
 }
